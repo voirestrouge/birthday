41df98234333f3aaa1e2232abf7383c9ea935163
Farriel Arrianta <farrielarrianta@gmail.com>
2024-10-27 21:16:27 +0700

    HBD

---
+++ script.js
@@ -0,0 +1,12 @@
+function cekJawaban() {
+    const jawaban = document.getElementById('jawaban').value.toLowerCase();
+    const pesan = document.getElementById('pesan');
+
+    // Ubah jawaban ini sesuai dengan teka-teki yang ingin kamu buat
+    if (jawaban === '7') {
+        // Jika benar, arahkan ke halaman hadiah.html
+        window.location.href = 'hadiah.html';
+    } else {
+        pesan.textContent = 'Jawaban salah, coba lagi!';
+    }
+}
